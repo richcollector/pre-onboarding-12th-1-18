1d3d04905b50c4fd96a4538cb06b272fd2cbaca4
junhui324 <junhuijang00@gmail.com>
2023-08-24 14:54:30 +0900

    fix: lazy 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ROUTES from './constants/Routes';
-import Main from './pages/Main';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Todo from './pages/TodoList';
-import AuthRoute from './components/Route/AuthRoute';
-import PublicRoute from './components/Route/PublicRoute';
+import { lazy, Suspense } from 'react';
 
-// import { lazy } from 'react';
-
-// const Main = lazy(() => import('./pages/Main'));
-// const SignUp = lazy(() => import('./pages/SignUp'));
-// const SignIn = lazy(() => import('./pages/SignIn'));
-// const Todo = lazy(() => import('./pages/TodoList'));
+const Main = lazy(() => import('./pages/Main'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const SignIn = lazy(() => import('./pages/SignIn'));
+const Todo = lazy(() => import('./pages/TodoList'));
+const AuthRoute = lazy(() => import('./components/Route/AuthRoute'));
+const PublicRoute = lazy(() => import('./components/Route/PublicRoute'));
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Main />} />
-				<Route element={<PublicRoute />}>
-					<Route path={ROUTES.SIGNUP} element={<SignUp />} />
-					<Route path={ROUTES.SIGNIN} element={<SignIn />} />
-				</Route>
-				<Route element={<AuthRoute />}>
-					<Route path={ROUTES.TODOLIST} element={<Todo />} />
-				</Route>
-			</Routes>
+			<Suspense fallback="Loading">
+				<Routes>
+					<Route path="/" element={<Main />} />
+					<Route element={<PublicRoute />}>
+						<Route path={ROUTES.SIGNUP} element={<SignUp />} />
+						<Route path={ROUTES.SIGNIN} element={<SignIn />} />
+					</Route>
+					<Route element={<AuthRoute />}>
+						<Route path={ROUTES.TODOLIST} element={<Todo />} />
+					</Route>
+				</Routes>
+			</Suspense>
 		</BrowserRouter>
 	);
 }
