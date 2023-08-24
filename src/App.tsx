ae19d6e41bd7105ecaaebf1df2a35c5521566902
Jungkyu Yang <didwjd8590412@gmail.com>
2023-08-24 12:19:50 +0900

    feat(route): 기본 route 결로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import ROUTES from './constants/routes';
+import MainPage from './pages/MainPage';
+import SignUpPage from './pages/SignUpPage';
+import SignInPage from './pages/SignInPage';
+import TodoPage from './pages/TodoPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Routes>
+			<Route path={ROUTES.MAIN} element={<MainPage />} />
+			<Route path={ROUTES.SIGNUP} element={<SignUpPage />} />
+			<Route path={ROUTES.SIGNIN} element={<SignInPage />} />
+			<Route path={ROUTES.TODO} element={<TodoPage />} />
+			<Route path="*" element={<NotFoundPage />} />
+		</Routes>
 	);
 }
 
---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,5 @@
+function TodoPage() {
+	return <div>TodoPage</div>;
+}
+
+export default TodoPage;
